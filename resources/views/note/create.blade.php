e42e00fb3cc2df56338dd6294a61e055b60f1d71
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2025-04-09 12:08:19 +0300

    dev: define CRUD views.

--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -1,3 +1,15 @@
 <x-layout>
-    <h1>Create note.</h1>
+    <div class="note-container single-note">
+        <h1>Create new note</h1>
+        <!-- When submitting this form, route to note.store to store it in the db. -->
+        <form action="{{ route('note.store') }}" method="POST" class="note">
+            <textarea name="note" rows="10" class="note-body" placeholder="Enter your note here"></textarea>
+            <div class="note-buttons">
+                <!-- Go back to the note list. -->
+                <a href="{{ route('note.index') }}" class="note-cancel-button">Cancel</a>
+                <!-- Invoke POST method. -->
+                <button class="note-submit-button">Submit</button>
+            </div>
+        </form>
+    </div>
 </x-layout>
