03052a36fa03b167e72a3c61e6f46bf6d2c60349
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2025-04-11 21:14:59 +0300

    update: edit styles

--- resources/views/body_part_type/edit.blade.php
+++ resources/views/body_part_type/edit.blade.php
@@ -1,25 +1,27 @@
-<div>
-    <h1>Edit Body Part Type</h1>
-
-    <form action="{{ route('body_part_type.update', $bodyPartType->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-
-        <div>
-            <label for="name">Name</label>
-            <textarea name="name" id="name" required>{{ $bodyPartType->name }}</textarea>
+<x-layout>
+    <div class="body-part-type-box-container">
+        <div class="body-part-type-box info">
+            <h1>Edit Body Part Type</h1>
+            <form action="{{ route('body_part_type.update', $bodyPartType->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div>
+                    <label for="name">Name</label>
+                    <br>
+                    <textarea name="name" id="name" required>{{ $bodyPartType->name }}</textarea>
+                </div>
+                <div>
+                    <label for="expiration_period_minutes">Expiration Period</label>
+                    <br>
+                    <input type="number" name="expiration_period_minutes" id="expiration_period_minutes" value="{{ $bodyPartType->expiration_period_minutes }}" required>
+                </div>
+                <div>
+                    <label for="description">Description</label>
+                    <br>
+                    <textarea name="description" id="description" rows="3" required>{{ $bodyPartType->description }}</textarea>
+                </div>
+                <button class="crud-button update" type="submit">Update</button>
+            </form>
         </div>
-
-        <div>
-            <label for="expiration_period_minutes">Expiration Period</label>
-            <input type="number" name="expiration_period_minutes" id="expiration_period_minutes" value="{{ $bodyPartType->expiration_period_minutes }}" required>
-        </div>
-
-        <div>
-            <label for="description">Description</label>
-            <textarea name="description" id="description" rows="3" required>{{ $bodyPartType->description }}</textarea>
-        </div>
-
-        <button type="submit">Update</button>
-    </form>
-</div>
+    </div>    
+</x-layout>
