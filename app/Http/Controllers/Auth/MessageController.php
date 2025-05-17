bd4eb88f56af5cd8bbce92e6255dd465ecd20e26
Domantas Bieliūnas <domantas231@gmail.com>
2025-05-17 17:42:11 +0300

    add: added messages

---
+++ app/Http/Controllers/Auth/MessageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $messages = Message::all();
+        return view('message.index')->with('messages', $messages);
+    }
+}
