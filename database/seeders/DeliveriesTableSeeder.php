9e89ece82a713767823ae438237b7ed284d8a8d8
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2025-05-19 12:55:01 +0300

    add: more delivery routes

--- database/seeders/DeliveriesTableSeeder.php
+++ database/seeders/DeliveriesTableSeeder.php
@@ -34,5 +34,43 @@ class DeliveriesTableSeeder extends Seeder
             'current_location_coordinate_id' => null,
             'generated_route_id' => null,
         ]);
+
+        // Mano namas
+        $pickup = Coordinate::create([
+            'latitude' => 54.891989432731,
+            'longitude' => 24.0015467116237
+        ]);
+
+        // Autoservisas
+        $drop = Coordinate::create([
+            'latitude' => 54.919872841581864, 
+            'longitude' => 23.948425927667238
+        ]);
+
+        Delivery::create([
+            'pickup_point_coordinate_id' => $pickup->id,
+            'drop_point_coordinate_id' => $drop->id,
+            'current_location_coordinate_id' => null,
+            'generated_route_id' => null,
+        ]);
+
+        // KTU
+        $pickup = Coordinate::create([
+            'latitude' => 54.89968425015072,
+            'longitude' => 23.961933117768396
+        ]);
+
+        // Profkė
+        $drop = Coordinate::create([
+            'latitude' => 54.9205441510663, 
+            'longitude' => 23.99381522946014
+        ]);
+
+        Delivery::create([
+            'pickup_point_coordinate_id' => $pickup->id,
+            'drop_point_coordinate_id' => $drop->id,
+            'current_location_coordinate_id' => null,
+            'generated_route_id' => null,
+        ]);
     }
 }
